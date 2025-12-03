ed2eac5111d3dc1d64807d06a933ebc5c970e599
Silva Tech Nexus <silvatechnexus@gmail.com>
2025-12-03 14:12:44 +0300

    Update owner and bot configuration details

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ const path = require("path");
 module.exports = { 
     SESSION_ID: process.env.SESSION_ID || '',
     PREFIX: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇",
-    OWNER_NUMBER : process.env.OWNER_NUMBER || "",  // put only one number
+    OWNER_NAME: process.env.OWNER_NAME || "SILVA",
+    OWNER_NUMBER : process.env.OWNER_NUMBER || "254755257907",  // put only one number
     SUDO_NUMBERS : process.env.SUDO_NUMBERS || "", // can be multiple numbers separated by commas
-    BOT_NAME : process.env.BOT_NAME || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃',
-    FOOTER : process.env.FOOTER || 'ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪғᴛᴇᴅ ᴛᴇᴄʜ',
-    CAPTION : process.env.CAPTION || '©𝟐𝟎𝟐𝟒 𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃 𝐕𝟓',
+    BOT_NAME : process.env.BOT_NAME || 'SILVA',
+    FOOTER : process.env.FOOTER || 'SILVA',
+    CAPTION : process.env.CAPTION || 'SILVA',
     VERSION: process.env.VERSION || '5.0.0',
-    BOT_PIC : process.env.BOT_PIC || 'https://gitcdn.giftedtech.co.ke/image/AZO_image.jpg',            
+    BOT_PIC : process.env.BOT_PIC || 'https://files.catbox.moe/5uli5p.jpeg',            
     MODE: process.env.MODE || "public",
     PM_PERMIT: process.env.PM_PERMIT || 'false',
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     TIME_ZONE: process.env.TIME_ZONE || "Africa/Nairobi",
-    DM_PRESENCE : process.env.DM_PRESENCE || 'online', // recording/typing/online/offline
-    GC_PRESENCE : process.env.GC_PRESENCE || 'online', // recording/typing/online/offline
+    DM_PRESENCE : process.env.DM_PRESENCE || 'recording', // recording/typing/online/offline
+    GC_PRESENCE : process.env.GC_PRESENCE || 'typing', // recording/typing/online/offline
     CHATBOT : process.env.CHATBOT || 'false', // can be true/audio/false   
     CHATBOT_MODE : process.env.CHATBOT_MODE || 'inbox', // can be inbox/groups/allchats
     STARTING_MESSAGE : process.env.STARTING_MESSAGE || "true",
@@ -28,7 +28,7 @@ module.exports = {
     ANTICALL : process.env.ANTICALL || 'false', // (decline/true)/block/false
     ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Active. 📵 No Calls Allowed!_*",
     WELCOME_MESSAGE : process.env.WELCOME_MESSAGE || 'false',
-    ANTILINK : process.env.ANTILINK || 'false', // or delete or kick or true
+    ANTILINK : process.env.ANTILINK || 'true', // or delete or kick or true
     AUTO_LIKE_STATUS : process.env.AUTO_LIKE_STATUS || 'true',
     AUTO_READ_STATUS : process.env.AUTO_READ_STATUS || 'true',
     STATUS_LIKE_EMOJIS : process.env.STATUS_LIKE_EMOJIS || "💛,❤️,💜,🤍,💙",
@@ -39,12 +39,12 @@ module.exports = {
     AUTO_READ_MESSAGES : process.env.AUTO_READ_MESSAGES || 'false', // true/commands/false
     AUTO_BIO : process.env.AUTO_BIO || 'false',
     AUTO_BLOCK: process.env.AUTO_BLOCK || '212,233',
-    YT: process.env.YT || 'youtube.com/@giftedtechnexus',
-    NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363408839929349@newsletter',
-    NEWSLETTER_URL: process.env.NEWSLETTER_URL || 'https://whatsapp.com/channel/0029Vb3hlgX5kg7G0nFggl0Y',
-    BOT_REPO: process.env.BOT_REPO || 'mauricegift/gifted-md',
-    PACK_NAME: process.env.PACK_NAME || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐌𝐃',
-    PACK_AUTHOR: process.env.PACK_AUTHOR || '𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇'
+    YT: process.env.YT || 'youtube.com/@silvatechnexus',
+    NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363200367779016@newsletter',
+    NEWSLETTER_URL: process.env.NEWSLETTER_URL || 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v',
+    BOT_REPO: process.env.BOT_REPO || 'SilvaTechB/silva-md-bot',
+    PACK_NAME: process.env.PACK_NAME || 'silva',
+    PACK_AUTHOR: process.env.PACK_AUTHOR || 'silva'
 };
 
 let fileName = require.resolve(__filename);
